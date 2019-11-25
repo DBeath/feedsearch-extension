bc54104df75216d90bff0b12c66c49d49158cad7
Dave Beath <davidgbeath@gmail.com>
2019-11-25 08:19:30 +0100

    Update

--- popup.js
+++ popup.js
@@ -1,20 +1,20 @@
 
-let changeColor = document.getElementById('changeColor');
+// let changeColor = document.getElementById('changeColor');
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+// chrome.storage.sync.get('color', function(data) {
+//   changeColor.style.backgroundColor = data.color;
+//   changeColor.setAttribute('value', data.color);
+// });
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  console.log(`Color: ${color}`);
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-      tabs[0].id,
-      {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
-};
+// changeColor.onclick = function(element) {
+//   let color = element.target.value;
+//   console.log(`Color: ${color}`);
+//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     chrome.tabs.executeScript(
+//       tabs[0].id,
+//       {code: 'document.body.style.backgroundColor = "' + color + '";'});
+//   });
+// };
 
 let queryFeeds = document.getElementById('queryFeeds');
 
